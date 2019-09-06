21c09a82451a099bcbf36bbcd420f06c79f872a8
GuybrushThreepwood-Dev <54991104+GuybrushThreepwood-Dev@users.noreply.github.com>
2019-09-06 15:38:38 +0100

    Add files via upload

---
+++ DisableWindowsUpdates.ps1
@@ -0,0 +1,7 @@
+﻿# Disable Windows Updates - 2016 and upwards
+
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\ -Name WindowsUpdate
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\ -Name AU
+Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name AUOptions -Value 1
+Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name AUOptions -Value 1
